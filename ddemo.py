cdbd13cb3cebef8dfdd48139dbf84aa070e7c553
fns11 <105477971+fns11@users.noreply.github.com>
2022-05-13 13:33:27 -0400

    Update ddemo.py

--- ddemo.py
+++ ddemo.py
@@ -30,62 +30,62 @@ img_lottie_animation = Image.open("demo_image1.png")
 
 # ---- HEADER SECTION ----
 with st.container():
-    st.subheader("Hi, I am Sven :wave:")
-    st.title("A Data Analyst From Germany")
+    st.subheader("Hi, I am Dominica :wave:")
+    st.title("A Veteran Counselor that's here to assist you")
     st.write(
-        "I am passionate about finding ways to use Python and VBA to be more efficient and effective in business settings."
+        "Q: Where did you sleep last night, tell us all about it."
     )
-    st.write("[Learn More >](https://pythonandvba.com)")
+    st.write("[Learn More >](https://youtu.be/ix1BUgTztGU)")
 
 # ---- WHAT I DO ----
 with st.container():
     st.write("---")
     left_column, right_column = st.columns(2)
     with left_column:
-        st.header("What I do")
+        st.header("What we do to get you the assistance you need")
         st.write("##")
         st.write(
             """
             On my YouTube channel I am creating tutorials for people who:
-            - are looking for a way to leverage the power of Python in their day-to-day work.
-            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
-            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
-            - are working with Excel and found themselves thinking - "there has to be a better way."
-            If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
+            - are looking for a way to looking for housing.
+            - are struggling with life situations and want a change.
+            - want to learn new skills and trades.
+            - are trying to work and thinking  "there has to be a better way."
+            If this sounds interesting to you, consider subscribing and and filling out the forms below.
             """
         )
-        st.write("[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
+        st.write("[YouTube Channel >](https://youtu.be/zjTE48Ab9J0)")
     with right_column:
         st_lottie(lottie_coding, height=300, key="coding")
 
 # ---- PROJECTS ----
 with st.container():
     st.write("---")
-    st.header("My Projects")
+    st.header("Forms to Fill out below ")
     st.write("##")
     image_column, text_column = st.columns((1, 2))
     with image_column:
         st.image(img_lottie_animation)
     with text_column:
-        st.subheader("Integrate Lottie Animations Inside Your Streamlit App")
+        st.subheader("These forms below will better let us know your situation and if you qualify")
         st.write(
             """
-            Learn how to use Lottie Files in Streamlit!
-            Animations make our web app more engaging and fun, and Lottie Files are the easiest way to do it!
-            In this tutorial, I'll show you exactly how to do it
+            Sign up today!
+            We have links to answer questions you may have!
+            FAQs
             """
         )
-        st.markdown("[Watch Video...](https://youtu.be/TXSOitGoINE)")
+        st.markdown("[Watch Video...](https://youtu.be/RlKJDwViNKs)")
 with st.container():
     image_column, text_column = st.columns((1, 2))
     with image_column:
         st.image(img_contact_form)
     with text_column:
-        st.subheader("How To Add A Contact Form To Your Streamlit App")
+        st.subheader("How To instructions if you have questions about application process")
         st.write(
             """
-            Want to add a contact form to your Streamlit website?
-            In this video, I'm going to show you how to implement a contact form in your Streamlit app using the free service ‘Form Submit’.
+            Forms and Information below
+            FAQs’.
             """
         )
         st.markdown("[Watch Video...](https://youtu.be/FOULV9Xij_8)")
@@ -93,12 +93,12 @@ with st.container():
 # ---- CONTACT ----
 with st.container():
     st.write("---")
-    st.header("Get In Touch With Me!")
+    st.header("Get In Touch With Me if you have any questions!") 
     st.write("##")
 
     # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
     contact_form = """
-    <form action="https://formsubmit.co/YOUR@MAIL.COM" method="POST">
+    <form action="https://formsubmit.co/dominica.hewett@uwforsyth.org" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>

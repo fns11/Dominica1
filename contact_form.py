fd89698b417a40f40f0824e180ef2f624595a6b3
fns11 <105477971+fns11@users.noreply.github.com>
2022-05-16 23:34:03 -0400

    Add files via upload

---
+++ contact_form.py
@@ -0,0 +1,18 @@
+.css-d1b1ld.edgvbvh6
+{
+    visibility: hidden;
+    
+
+.css-1v8iw71.eknhn3m4
+{
+    visibility: hidden;
+}
+</stlyle>
+""", unsafe_allow_html=True)
+st.markdown('<h1 style = 'text-align: center;'>User 
+with st.form('Form 2')
+f_name=col1.text_input('First Name')
+l_name=col2.text_input('Last Name')
+st.text_input('Email Address')
+st.text_input('Confirm Password')
+st.form_submit_botton

f62e13686498dade6c2b076741b7120946c66738
fns11 <105477971+fns11@users.noreply.github.com>
2022-06-01 11:48:38 -0400

    Update ddemo.py

--- ddemo.py
+++ ddemo.py
@@ -2,10 +2,8 @@ import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
 from PIL import Image
-from streamlit.cli import main
+
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
-
-
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 

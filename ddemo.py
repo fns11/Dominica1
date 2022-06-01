fc9cfd55b14abdf87e8ae0a08b60ec13d3c0eebc
fns11 <105477971+fns11@users.noreply.github.com>
2022-06-01 11:46:23 -0400

    Update ddemo.py

--- ddemo.py
+++ ddemo.py
@@ -2,7 +2,7 @@ import requests
 import streamlit as st
 from streamlit_lottie import st_lottie
 from PIL import Image
-
+from streamlit.cli import main
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 
 

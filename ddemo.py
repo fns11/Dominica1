1f863d199d4b05cafa43115c997cd27afba1cfc8
fns11 <105477971+fns11@users.noreply.github.com>
2022-06-01 11:24:53 -0400

    Update ddemo.py

--- ddemo.py
+++ ddemo.py
@@ -15,7 +15,7 @@ if theme in ["light", "dark"]:
 
 
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+        st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
 
 def load_lottieurl(url):

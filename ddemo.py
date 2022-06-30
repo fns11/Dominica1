c97143ae1b72bb5efbc945ef0d6a138a942a7be6
Code_House <105477971+fns11@users.noreply.github.com>
2022-06-30 11:40:07 -0400

    Update ddemo.py

--- ddemo.py
+++ ddemo.py
@@ -132,15 +132,6 @@ st.write("---")
 
 st.title("SSVF Forms Selection")
 
-with st.form('Form1'):
-    st.selectbox('Select your living status', ['Renting', 'Looking to Rent', 'Homeless'], key=1)
-    st.slider(label='Select intensity', min_value=0, max_value=100, key=4)
-    submitted1 = st.form_submit_button('Submit 1')
-             
-my_form = st.form(key = "form1")
-name = my_form.text_input(label = "Enter your First and Last Name")
-number = my_form.slider("Enter your age", min_value=10, max_value = 100 )
-submit = my_form.form_submit_button(label = "Submit this form")
 
 
               

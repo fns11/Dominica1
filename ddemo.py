104d898edc5778b17d34aacce9853499ea73ba3b
Code_House <105477971+fns11@users.noreply.github.com>
2022-06-30 10:09:18 -0400

    Update ddemo.py

--- ddemo.py
+++ ddemo.py
@@ -39,6 +39,10 @@ with st.container():
         st.slider(label='Select intensity', min_value=0, max_value=100, key=4)
         submitted1 = st.form_submit_button('Submit 1')
         
+        st.markdown(''' Click the links below to Download and Fill out Apllications
+        
+        ''')
+        
     st.write("[Learn More & Download SSVF HP Screener Application Here>](https://docs.google.com/document/d/e/2PACX-1vSp_1zm6iAvep64Ib8Nw5MN8lRsDynr4g2NaAe21o_8umNX88RRgEsRt83Y3oEicfpFRL0dgR56h7_P/pub)")
 
     st.write("[Learn More & Download Intake Packet SSVF  Application Here>](https://docs.google.com/document/d/e/2PACX-1vSp_1zm6iAvep64Ib8Nw5MN8lRsDynr4g2NaAe21o_8umNX88RRgEsRt83Y3oEicfpFRL0dgR56h7_P/pub)")

d180cf1e556ae5d4479be565e382dacd724b0412
Code_House <105477971+fns11@users.noreply.github.com>
2022-06-30 10:11:57 -0400

    Update ddemo.py

--- ddemo.py
+++ ddemo.py
@@ -33,13 +33,13 @@ with st.container():
     st.write(
         "Q: Where did you sleep last night, tell us all about it."
     )
-    
+st.markdown('Forms Below')
     with st.form('Form1'):
         st.selectbox('Select your living status', ['Renting', 'Looking to Rent'], key=1)
         st.slider(label='Select intensity', min_value=0, max_value=100, key=4)
         submitted1 = st.form_submit_button('Submit 1')
         
-     st.write(--)
+   
         
         st.markdown(''' Click the links below to Download and Fill out Apllications
         
